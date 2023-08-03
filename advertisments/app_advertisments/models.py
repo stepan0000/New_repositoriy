befe847d6984c899e97c8da19e2fe2de0de5aedc
Stepanich0000 <stepanich.blackbeard@gmail.com>
2023-08-03 17:17:50 +0300

    seventeenth commit

--- advertisments/app_advertisments/models.py
+++ advertisments/app_advertisments/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
 # Create your models here.
+class Advertisment(models.Model):
+    title = models.CharField("название",max_length=120)
+    descriptions = models.TextField("описание")
+    price = models.DecimalField("цена",max_digits=9,decimal_places=2)
+    trades = models.BooleanField("торг",help_text="Если хотим торговаться")
+    date_now = models.DateField("Создание дата",auto_now_add= True)
+    date_update = models.DateField("Обновление дата",auto_now = True)
+    class Meta:
+        db_table = "advertisments"
+    def __str__(self):
+        return f"id={self.pk},title={self.title},price={self.price}"
